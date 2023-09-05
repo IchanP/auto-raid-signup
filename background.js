2cc21498b2c3716ef99d3f28b926e757e81796a2
Pontus Grandin <pg222pb@student.lnu.se>
2023-09-05 18:42:53 +0200

    add redirect to settings when action is clicked

--- background.js
+++ background.js
@@ -1,37 +1,52 @@
+/* eslint-disable no-undef */
 const navigateToUrl = 'https://myanimelist.net/profile/Oskz'
 let createdTab
 importScripts('main.js')
 
 chrome.alarms.create('testing-again', {
-    when: Date.now() + 5000
-});
+  when: Date.now() + 5000
+})
 
 initialize()
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
-    console.log(Date.now() + alarm.name)
-    chrome.alarms.create('testing-again', {
-        when: Date.now() + 1500000
-    })
+  console.log(Date.now() + alarm.name)
+  chrome.alarms.create('testing-again', {
+    when: Date.now() + 1500000
+  })
 })
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (tabId === createdTab.id && changeInfo.url) {
-        chrome.scripting.executeScript(
-            {
-                target: { tabId: createdTab.id },
-                files: ['./scripts/mal.js']
+  if (tabId === createdTab.id && changeInfo.url) {
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: createdTab.id },
+        files: ['./scripts/mal.js']
 
-            }
-        )
-    }
+      }
+    )
+  }
 })
 
-async function justTestingFromMain(message) {
-    console.log(message)
-    createdTab = await chrome.tabs.create({
-        active: false,
-        url: navigateToUrl
-    }
-    )
+/**
+ *
+ * @param message
+ */
+async function justTestingFromMain (message) {
+  console.log(message)
+  createdTab = await chrome.tabs.create({
+    active: false,
+    url: navigateToUrl
+  }
+  )
 }
+
+
+/**
+ *
+ */
+const extensionIconClickListener = () => {
+  chrome.tabs.create({ url: './popup/popup.html' })
+}
+
+chrome.action.onClicked.addListener(extensionIconClickListener)
